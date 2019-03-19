3fa2e4c77e6c98239b34c0103439d41d809a53d5
Ringo Singson <ringosingson@live.com>
2019-03-20 03:11:28 +0530

    13 nachan nahetthemloule doc sim in nodeapi

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path") //path is a path of node
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark(
+          limit: 5
+          sort: { order: DESC, fields: [frontmatter___date] }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(results => {
+      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: `/posts${node.frontmatter.slug}`,
+          component: path.resolve(`src/components/postLayout.js`),
+          context: {
+            slug: node.frontmatter.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
+}
